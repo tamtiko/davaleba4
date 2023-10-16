02833cc016b7b8b89a3552bba94ddfa03e2a8cec
tamtiko <147196741+tamtiko@users.noreply.github.com>
2023-10-16 20:54:11 +0400

    davaleba4.py

--- davaleba4.py
+++ davaleba4.py
@@ -1,20 +1,27 @@
-class Person:
-    def __init__(self, name: str, surname: str, age: int):
+class Student:
+    university = "Tbilisi State Medical University""
+
+    def __init__(self, name: str, grade: int, age: int):
         self.name = name
-        self.surname = surname
+        self.grade = grade
         self.age = age
 
-class PersonMixin:
     def __str__(self):
-        return f"Name: {self.name}, Surname: {self.surname}, Age: {self.age}"
+        return f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}"
 
-class Student(Person, PersonMixin):
-    def __init__(self, name: str, surname: str, age: int, university: str):
-        super().__init__(name, surname, age)
-        self.university = university
-    def __str__(self):
-        return f"{super().__str__()}, University: {self.university}"    
+    @property
+    def is_passing(self):
+        return self.grade > 60
 
-study = Student("Tamta", "Kuloshvili", 25, "Tbilisi State Medical University")
+    def increase_grade(self, increase_by):
+        self.grade += increase_by
+        
+study = Student("Nick", 70, 10)
 
-print(study) 
+print(study.university)
+
+print(study.is_passing)
+
+study.increase_grade(10)
+
+print(study.grade)
